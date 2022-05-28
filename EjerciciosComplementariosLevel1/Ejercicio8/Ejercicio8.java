afcb5e3e7e4a81e019aaa30db91f75cfa025cfec
unknown <marianaalmada15@outlook.com>
2022-05-28 19:56:45 -0300

    ejercicio 8 modificado

--- EjerciciosComplementariosLevel1/Ejercicio8/Ejercicio8.java
+++ EjerciciosComplementariosLevel1/Ejercicio8/Ejercicio8.java
@@ -6,18 +6,19 @@ public class Ejercicio8 {
     
     public static void main(String[] args) {
         
-        System.out.println("Ingresar nombre completo");
         Scanner datos = new Scanner(System.in);
-        String nombreCom = datos.nextLine();
 
-        System.out.println("Ingresar edad");
-        int edad = datos.nextInt();
+        System.out.println("Ingresar nombre completo");
+        String nombreCom = datos.nextLine();
 
         System.out.println("Ingresar direccion");
         String direccion = datos.nextLine();
 
         System.out.println("Ingresar ciudad");
         String ciudad = datos.nextLine();
+
+        System.out.println("Ingresar edad");
+        int edad = datos.nextInt();
 
         Persona persona = new Persona(nombreCom, edad, direccion, ciudad);
 
