c484078155462033b7f4367961892be9022b72b9
unknown <marianaalmada15@outlook.com>
2022-06-04 14:03:36 -0300

    ejercicio 8 modificado

--- EjerciciosComplementariosLevel2/Ejercicio5.java
+++ EjerciciosComplementariosLevel2/Ejercicio5.java
@@ -7,13 +7,13 @@ public class Ejercicio5 {
     
     public static void main(String[] args) {
         
-        ArrayList<Integer> horasTrabajadas = new ArrayList<>(List.of(6, 7, 8, 4, 5));
-        ArrayList<Integer> valorXHora = new ArrayList<>(List.of(350, 345, 550, 600, 320));
+        List<Integer> horasTrabajadas = new ArrayList<>(List.of(6, 7, 8, 4, 5));
+        List<Integer> valorXHora = new ArrayList<>(List.of(350, 345, 550, 600, 320));
 
         calcularMontoTotal(horasTrabajadas, valorXHora);
     }
 
-    public static void calcularMontoTotal(ArrayList<Integer> horas, ArrayList<Integer> valor) {
+    public static void calcularMontoTotal(List<Integer> horas, List<Integer> valor) {
         List<Integer> totalXDia = new ArrayList<>();
         int cont = 0;
         int resultado;
