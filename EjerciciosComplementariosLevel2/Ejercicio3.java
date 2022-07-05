6b1ff99eb570ce9f12791b3f6354e49fac7de2ea
unknown <marianaalmada15@outlook.com>
2022-07-04 22:02:27 -0300

    valores de la baraja francesa agregados

--- EjerciciosComplementariosLevel2/Ejercicio3.java
+++ EjerciciosComplementariosLevel2/Ejercicio3.java
@@ -9,15 +9,20 @@ public class Ejercicio3 {
 
     public static void main(String[] args) {
 
-        List<Integer> numerosCartas = new ArrayList<>();
-        numerosCartas.add(2);
-        numerosCartas.add(3);
-        numerosCartas.add(4);
-        numerosCartas.add(5);
-        numerosCartas.add(6);
-        numerosCartas.add(7);
-        numerosCartas.add(9);
-        numerosCartas.add(10);
+        List<String> numerosCartas = new ArrayList<>();
+        numerosCartas.add("A");
+        numerosCartas.add("2");
+        numerosCartas.add("3");
+        numerosCartas.add("4");
+        numerosCartas.add("5");
+        numerosCartas.add("6");
+        numerosCartas.add("7");
+        numerosCartas.add("8");
+        numerosCartas.add("9");
+        numerosCartas.add("10");
+        numerosCartas.add("J");
+        numerosCartas.add("Q");
+        numerosCartas.add("K");
 
         System.out.println(numerosCartas);
 
